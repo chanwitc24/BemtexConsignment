fae25b9de941263407d02dbbc975a2689d25ccbb
Chanwit Chummung <chanwitchummung@Chanwits-MacBook-Pro.local>
2017-11-29 14:20:03 +0700

    Initial Commit

---
+++ Consignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Consignment
+//
+//  Created by Chanwit Chummung on 11/29/2560 BE.
+//  Copyright © 2560 Chanwit Chummung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
